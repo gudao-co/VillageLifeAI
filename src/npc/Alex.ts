9771cedf74411a752cc507d27537af4e71c9b405
Theron <0x50Fc@gmail.com>
2025-07-17 18:04:43 +0800

    Add NPC data for Alex, Haley, Emily

---
+++ src/npc/Alex.ts
@@ -0,0 +1,21 @@
+import { Village } from "../def";
+
+export default {
+    name: 'Alex',
+    description: '前高中橄榄球明星，现在帮助祖母经营农场',
+    gender: Village.Gender.Male,
+    features: ['金发', '喜欢运动', '关心祖母'],
+    birthday: {
+        season: Village.GDateSeason.Summer,
+        day: 13,
+        year: 0,
+        weekday: Village.GDateWeekday.Sunday
+    },
+    like: ['蛋白棒', '哑铃', '牛排'],
+    dislike: ['鱼', '海藻', '蜗牛'],
+    address: {
+        name: Village.GLocationName.Town,
+        x: 35,
+        y: 42
+    }
+} as Village.NPC
---
+++ src/npc/Haley.ts
@@ -0,0 +1,21 @@
+import { Village } from "../def";
+
+export default {
+    name: 'Haley',
+    description: '热爱摄影和时尚的小镇女孩，有点娇生惯养',
+    gender: Village.Gender.Female,
+    features: ['金发', '喜欢拍照', '注重外表'],
+    birthday: {
+        season: Village.GDateSeason.Spring,
+        day: 14,
+        year: 0,
+        weekday: Village.GDateWeekday.Friday
+    },
+    like: ['向日葵', '粉色蛋糕', '钻石'],
+    dislike: ['蔬菜', '昆虫', '粘土'],
+    address: {
+        name: Village.GLocationName.Town,
+        x: 48,
+        y: 51
+    }
+} as Village.NPC
